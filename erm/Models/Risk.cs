62d288cb279b2390eb8cb8107c1073bd79a77b31
Турсунов Баходур <tursunovb18@gmail.com>
2024-06-02 22:38:38 +0500

    change: string properties

--- erm/Models/Risk.cs
+++ erm/Models/Risk.cs
@@ -2,8 +2,8 @@ namespace Erm.Models;
 
 public class Risk : BaseEntity
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; }= string.Empty;
+    public string Description { get; set; }= string.Empty;
     public int Probability { get; set; }
     public int Impact { get; set; }
 }
