041c58644bf3dc5319c5c416118c35ee4fdd1440
Турсунов Баходур <tursunovb18@gmail.com>
2024-06-02 22:36:50 +0500

    change: string properties

--- erm/Models/Project.cs
+++ erm/Models/Project.cs
@@ -2,10 +2,10 @@ namespace Erm.Models;
 
 public class Project : BaseEntity
 {
-    public string Name { get; set; }
+    public string Name { get; set; }= string.Empty;
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public string Sprints { get; set; }
-    public string Risks { get; set; }
-    public string AssignedTo { get; set; }
+    public string Sprints { get; set; }= string.Empty;
+    public string Risks { get; set; }= string.Empty;
+    public string AssignedTo { get; set; }= string.Empty;
 }
--- erm/Models/Worker.cs
+++ erm/Models/Worker.cs
@@ -2,6 +2,6 @@ namespace Erm.Models;
 
 public class Worker : Person
 {
-    public string Responsibility { get; set; }
+    public string Responsibility { get; set; }= string.Empty;
 }
 
