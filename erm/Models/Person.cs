df93d480430fc978e1379d77a9b62a40b72083e6
Турсунов Баходур <tursunovb18@gmail.com>
2024-06-02 22:31:53 +0500

    change string properties

--- erm/Models/Person.cs
+++ erm/Models/Person.cs
@@ -4,14 +4,14 @@ namespace Erm.Models
 {
     public abstract class Person : BaseEntity
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string? Address { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; }= string.Empty;
+        public string Address { get; set; }= string.Empty;
         public string FullName => $"{FirstName} {LastName}";
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string RefreshToken { get; set; }
-        public string Role { get; set; }
+        public string Username { get; set; }= string.Empty;
+        public string Password { get; set; }= string.Empty;
+        public string RefreshToken { get; set; }= string.Empty;
+        public string Role { get; set; }= string.Empty;
         public bool IsBlocked { get; }
     }
 }
